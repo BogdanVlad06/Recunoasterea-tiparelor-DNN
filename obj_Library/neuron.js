1846c47388478fe6bacdde400a53e3679397d8a7
GBVlad <guraubogdanvlad@gmail.com>
2024-10-17 12:15:08 +0300

    am inbunatatit logica

--- obj_Library/neuron.js
+++ obj_Library/neuron.js
@@ -1,6 +1,6 @@
 class neuron {
     constructor(input) {
-        this.z = 0;
+        this.weightedSum = 0;
         this.size = input.length;
         this.b = random(-1, 1);
         this.w = new Array(this.size);
@@ -14,20 +14,15 @@ class neuron {
         }
     }
 
-    activFunction(z) {
-        return Math.exp(z) / (1 + Math.exp(z));
-    }
-
-    feedforward(input) {
-        this.z = 0;
+    computeSum(input) {
+        this.weightedSum = 0;
         for (let i = 0; i < this.size; ++i) {
-            this.z += (this.w[i] * input[i]);
+            this.weightedSum += (this.w[i] * input[i]);
         }
-        this.z += this.b;
-        this.z = this.activFunction(this.z);
+        this.weightedSum += this.b;
     }
-
-    getOutput() {
-        return this.z;
+// ----------- Getters --------------
+    getActivation() {
+        return sigmoid(this.weightedSum)
     }
 }
