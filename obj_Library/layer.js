1ed931f78c9edd59ac27c27bc32a295c308e913b
GBVlad <guraubogdanvlad@gmail.com>
2024-12-07 18:59:56 +0200

    Layer class stable version

--- obj_Library/layer.js
+++ obj_Library/layer.js
@@ -2,46 +2,123 @@
 class Layer {
     constructor(numNeurons, connectionsToPrevL, activFunction = null) {
         this.neuronArr = new Array(numNeurons);
-        this.weightsMatrix = new Array(numNeurons);
-        this.biasesMatrix = new Array(numNeurons);
-        this.activationMatrix;
+        this.weightsArr = new Array(numNeurons);
+        this.biasesArr = new Array(numNeurons);
+        this.activationArr;
         this.activationFunction = activFunction;
+        this.gradientArr = new Array(numNeurons);
+        this.avgGradientArr = new Array(numNeurons).fill(0);
         
 
         this.genLayer(numNeurons, connectionsToPrevL);
     }
     
-    genLayer(numNeurons, connectionsToPrevL) {
+    /*genLayer(numNeurons, connectionsToPrevL) { // works!
         for (let i = 0; i < numNeurons; ++i) {
             this.neuronArr[i] = new Neuron(connectionsToPrevL);
 
-            this.weightsMatrix[i] = this.neuronArr[i].getWeights();
-            this.biasesMatrix = this.neuronArr[i].getBias();
+            this.weightsArr[i] = this.neuronArr[i].getWeights();
+            this.biasesArr[i] = this.neuronArr[i].getBias();
         }
+    }*/
+    // ------------------- ALTERNATIVE WAY WITHOUT NEURON OBJECTS -----------------------------
+    genLayer(numNeurons, connectionsToPrevL) {
+        const stdDev = Math.sqrt(2 / connectionsToPrevL); // He Initialization
+        this.weightsArr = Array.from({ length: numNeurons }, () =>
+            Array.from({ length: connectionsToPrevL }, () => (Math.random() * 2 - 1) * stdDev)
+        );
+        this.biasesArr = Array.from({ length: numNeurons }, () => 0); // Initialize biases to zero
+    }
+    // --------------------- UPDATES -----------------------
+    computeActivationFromMatrix(weightedSumMatrix) { // works!
+        let weightedSumArr = weightedSumMatrix.toArray();
+        weightedSumArr = weightedSumArr.flat();
+        this.activationArr = weightedSumArr.map(this.activationFunction);
     }
 
-    computeActivation(weightedSum) {
-        this.activationMatrix = math.map(weightedSum, this.activationFunction);
+    computeAvgGradientArr(scalar) {
+        this.avgGradientArr = this.avgGradientArr.map(value => value * scalar);
+    }
+
+    updateAvgGradientArr() {
+        this.avgGradientArr = math.add(this.avgGradientArr, this.gradientArr);
     }
     // ----------------- GETTERS ---------------------------
+    getWeightsArr() {
+        return this.weightsArr;
+    }
+
     getWeightsMatrix() {
-        return this.weightsMatrix;
+        return math.matrix(this.weightsArr);
     }
 
-    getBiasesMatrix() {
-        return this.biasesMatrix;
+    getBiasesArr() {
+        return this.biasesArr;
     }
 
-    getActivationMatrix() {
-        return this.activationMatrix;
+    getBiasesMatrix() { // needs formating for column matrix
+        return math.reshape(math.matrix(this.biasesArr), [this.biasesArr.length, 1]) ;
+    }
+
+    getActivationArr() {
+        return this.activationArr;
+    }
+
+    getActivationMatrix() { // needs formating for column matrix
+        return math.reshape(math.matrix(this.activationArr), [this.activationArr.length, 1]);
+    }
+
+    getActivationFunction() {
+        return this.activationFunction;
+    }
+
+    getGradientMatrix() { // column matrix
+        return math.reshape(math.matrix(this.gradientArr), [this.gradientArr.length, 1]);
+    }
+
+    getGradientArr() {
+        return this.gradientArr;
+    }
+
+    getAvgGradientMatrix() {
+        return math.reshape(math.matrix(this.avgGradientArr), [this.avgGradientArr.length, 1]);
     }
     // ----------------------- SETTERS ----------------------
     
-    setActivations(activArr) {
-        this.activationMatrix = activArr;
+    setActivationsArr(activArr) {
+        this.activationArr = activArr; //math.reshape(math.matrix(activArr), [activArr.length, 1]);
     }
 
-    setBiasesMatrix(newMatrix) {
-        this.biasesMatrix = newMatrix;
+    setGradientArr(grdArr) {
+        this.gradientArr = grdArr;
+    }
+
+    setGradientArrFromMatrix(grdMatrix) {
+        let newGradietArr = grdMatrix.toArray();
+        newGradietArr = newGradietArr.flat();
+        this.gradientArr = newGradietArr;
+    }
+
+    setBiasesArrFromMatrix(biasesMatrix) {
+        let newBiasesArr = biasesMatrix.toArray();
+        newBiasesArr = newBiasesArr.flat();
+        this.biasesArr = newBiasesArr;
+    }
+
+    setBiasesArr(newBiasesArr) {
+        this.biasesArr = newBiasesArr;
+    }
+
+    setWeightsArrFromMatrix(newWeightsMatrix) {
+        let newWeightsArr = newWeightsMatrix.toArray();
+        this.weightsArr = newWeightsArr;
+    }
+
+    setWeightsArr(newWeightsArr) {
+        this.biasesArr = newWeightsArr;
+    }
+
+    resetAvgGradientArr() {
+        this.avgGradientArr.fill(0);
     }
 }
