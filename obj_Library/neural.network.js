87b110a195509419b645f10d36cf82e69f60af40
GBVlad <guraubogdanvlad@gmail.com>
2024-12-07 13:37:00 +0200

    Made the FeedForward and ComputeCaseProb methods work with the layers according to their needs : Either math.js Matrixes or standard Arrays

--- obj_Library/neural.network.js
+++ obj_Library/neural.network.js
@@ -21,9 +21,9 @@ class NeuralNetwork {
         this.averageGradient[index] = new Array(size).fill(0);
     }
     
-    feedForward(input) { 
+    feedForward(input) {  // Works!
         this.network[0] = new Layer(0, 0);
-        this.network[0].setActivations(input);
+        this.network[0].setActivationsArr(input);
 
         let W_matrix, prevA_matrix, B_matrix, Z_matrix;
         for (let layerIndex = 1; layerIndex <= this.numHiddenLayers + 1; ++layerIndex) {
@@ -32,17 +32,15 @@ class NeuralNetwork {
             B_matrix = this.network[layerIndex].getBiasesMatrix();
 
             Z_matrix = math.add(math.multiply(W_matrix, prevA_matrix), B_matrix);
-
-            this.network[layerIndex].computeActivation(Z_matrix);
+            this.network[layerIndex].computeActivationFromMatrix(Z_matrix);
         }
     }
     
-    computeCaseProb() { // calculateOutput
-        //let outputActivationArr = this.getLayerActivationArr(this.numHiddenLayers + 1, "none");
+    computeCaseProb() { // Works!
         let outputLayerIndex = this.numHiddenLayers + 1;
-        let activations = this.network[outputLayerIndex].getActivationMatrix(),
-            expActivations = activations.map(Math.exp),
-            expSum = expActivations.reduce((acc, curr) => acc + curr, 0);
+        let activations = this.network[outputLayerIndex].getActivationArr();
+        let expActivations = activations.map(Math.exp);
+        let expSum = expActivations.reduce((acc, curr) => acc + curr, 0);
         
         for (let digit = 0; digit < this.outputSize; ++digit) {
             this.caseProbability[digit] = softmax(activations[digit], expSum);
@@ -54,14 +52,22 @@ class NeuralNetwork {
         return loss;
     }
     
-    backpropagate(label, actFunctionDerivate) {
+    backpropagate(label) {
         this.gradient[this.numHiddenLayers + 1] = this.caseProbability;
+        console.log(this.gradient[this.numHiddenLayers + 1]);
         this.gradient[this.numHiddenLayers + 1][label] -= 1;
         this.averageGradient[this.numHiddenLayers + 1] = math.add(this.averageGradient[this.numHiddenLayers + 1], this.gradient[this.numHiddenLayers + 1]);
         
         for (let layer = this.numHiddenLayers; layer > 0; --layer) {
+            console.log(layer);
+            let actFunctionDerivate = derivate(this.network[layer].getActivationFunction());
+            console.log("gradiente ", math.size(this.gradient[layer + 1]));
+            console.log("W_Matrix", math.size(this.network[layer + 1].getWeightsMatrix()));
+            
             let prevLayerDeltaGradients_matrix = math.multiply(this.gradient[layer + 1], this.network[layer + 1].getWeightsMatrix()),
                 derivateActivation_matrix = this.network[layer].getActivationMatrix().map(actFunctionDerivate);
+            
+                console.log("derivateMatrix " , math.size(derivateActivation_matrix), "prevL " , math.size(prevLayerDeltaGradients_matrix));
             this.gradient[layer] = math.multiply(derivateActivation_matrix, prevLayerDeltaGradients_matrix)
             
             this.averageGradient[layer] = math.add(this.averageGradient[layer], this.gradient[layer]);
@@ -90,6 +96,49 @@ class NeuralNetwork {
         this.learningRate = value;
     }
     // ---------------------- Utils -----------------------
+    train(decay, inputsArr, labelsArr, numCycles, batchSize = 10) {
+        const numInputs = inputsArr.length;
+        
+        for (let epoch = 0; epoch < numCycles; ++epoch) {
+            let epochLoss = 0;
+            this.learningRate = this.learningRate / (1 + (decay * epoch));
+            let correctPredictions = 0;
+            // Shuffle data each epoch for better convergence
+            const shuffledIndices = [...Array(numInputs).keys()].sort(() => Math.random() - 0.5);
+            
+            for (let startIdx = 0; startIdx < numInputs; startIdx += batchSize) {
+                // Accumulate gradients over the mini-batch
+                let batchLoss = 0;
+                
+                for (let i = startIdx; i < Math.min(startIdx + batchSize, numInputs); ++i) {
+                    const index = shuffledIndices[i];
+                    const input = inputsArr[index];
+                    const label = labelsArr[index];
+        
+                    this.feedForward(input);
+                    this.computeCaseProb();
+                    this.predict();
+                    
+                    let prediction = this.getPrediction();
+                    if (prediction == label) correctPredictions++;
+
+                    let exampleLoss = this.calculateLoss(label);
+                    batchLoss += exampleLoss;
+                    
+                    this.backpropagate(label);
+                }
+
+                // Update weights after each mini-batch
+                this.update(batchSize);
+
+                epochLoss += batchLoss / batchSize;
+            }
+            let accuracy = correctPredictions / numInputs;
+            updateMetrics(epochLoss / (numInputs / batchSize), accuracy);
+            console.log(`Epoch ${epoch + 1} Loss: ${epochLoss / (numInputs / batchSize)}, Accuracy: ${(accuracy * 100).toFixed(2)}%`);
+        }
+    }
+    
     predict() {
         let digitPrediction = -1, maxAct = 0;
     
