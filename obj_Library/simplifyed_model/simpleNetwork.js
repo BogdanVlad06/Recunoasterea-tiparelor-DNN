accb2ff9398de3bfc7b1318ede0ef3959d0ac3e3
BogdanVlad06 <guraubogdanvlad@gmail.com>
2025-01-13 22:11:47 +0200

    worked on the SGD method : made the backpropagate method & costDerivative

--- obj_Library/simplifyed_model/simpleNetwork.js
+++ obj_Library/simplifyed_model/simpleNetwork.js
@@ -1,7 +1,7 @@
 class SmpNetwork {
     constructor(size) {
-        this.size = size; // hid + output
-        this.network = new Array(size + 2);
+        this.size = size; // hid + output (== index of Output Layer)
+        this.network = new Array(size + 1); // 0 is input layer
         
     }
 
@@ -38,6 +38,50 @@ class SmpNetwork {
             //this.evaluate()
         }
     }
+
+    update(miniBatch, learningRate, trainingImages, trainingLabels) {
+        let getAvgGradientMatrix
+        for (let i = 0; i < miniBatch.length; ++i) {
+            // backpropagate
+        }
+
+    }
+
+    backpropagate(input, label) {
+        // forward pass
+        let activation = input;
+        let activations = [];
+        let weightedSums = [];
+        activation.push(null);
+        weightedSums.push(null);
+        for (let layerIndex = 1; layerIndex <= this.size; ++layerIndex) {
+            let w = this.network[layerIndex].getWeights();
+            let b = this.network[layerIndex].getBiases();
+            let weightedSum = math.add(math.multiply(w, activation), b);
+            activation = math.map(weightedSum, this.network[layerIndex].getActivationFunction());
+            weightedSums.push(weightedSum);
+            activations.push(activation);
+        }
+        // backward pass
+        let delta = math.dotMultiply(
+                    this.costDerivative(activations[this.size], label)
+                    , math.map(weightedSums[this.size], derivate(this.network[this.size].getActivationFunction())));
+        this.network[this.size].accumulateGradient(delta);
+        for (let layerIndex = this.size - 1; layerIndex > 0; --layerIndex) {
+            let w = this.network[layerIndex + 1].getWeights(); // w of L + 1
+            let primeWs = math.map(weightedSums[layerIndex], derivate(this.network[layerIndex].getActivationFunction()));
+            delta = math.dotMultiply(math.multiply(math.transpose(w), delta), primeWs);
+            this.network[layerIndex].accumulateGradient(delta);
+        }
+
+    }
+
+    costDerivative(outputActivations, label) {
+        let y = math.matrix(math.zeros(10, 1)); // hot encoded
+        y.set([label, 0], 1);
+        return math.multiply(math.substract(outputActivations, y), 2);
+    }
+    ////////////////////////////////////////////////////////////////////////
     
     computeCaseProb() { // Works!
         let outputLayerIndex = this.numHiddenLayers + 1;
@@ -85,12 +129,12 @@ class SmpNetwork {
             let prev_activation_matrix = this.network[layer - 1].getActivationMatrix();
             
             let newBiasesMatrix = math.subtract(biases_matrix, error_matrix);
-            
             this.network[layer].setBiasesArrFromMatrix(newBiasesMatrix);
+            
             let weightGradient_matrix = math.multiply(error_matrix, math.transpose(prev_activation_matrix));
             let newWeightsMatrix = math.subtract(weights_matrix, weightGradient_matrix);
-
             this.network[layer].setWeightsArrFromMatrix(newWeightsMatrix);
+
             this.network[layer].resetAvgGradientArr();
         }
     }
