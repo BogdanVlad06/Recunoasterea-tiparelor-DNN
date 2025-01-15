eb7675ae8ba345ca2bd6e6cb90006d5776275ce1
GBVlad <guraubogdanvlad@gmail.com>
2025-01-15 10:15:38 +0200

    couple modifications, issue with getting NaN after training :/ Needs to be checked if functioning corectly

--- obj_Library/simplifyed_model/simpleLayer.js
+++ obj_Library/simplifyed_model/simpleLayer.js
@@ -3,7 +3,7 @@ class SmpLayer {
         this.activFunction = activFunction
         this.weights; // Li x Li - 1
         this.biases; // Li x 1
-        this.avgWGradients = math.matrix(math.zeros(numNeurons, 1));
+        this.avgWGradients = math.matrix(math.zeros(numNeurons, connectionsToPrevL));
         this.avgBGradients = math.matrix(math.zeros(numNeurons, 1));
 
         this.basicInit(numNeurons, connectionsToPrevL);
@@ -12,23 +12,40 @@ class SmpLayer {
     basicInit(numNeurons, connectionsToPrevL) { // Initialize weights and biases with random values
         this.weights = math.random([numNeurons, connectionsToPrevL], -1, 1);
         this.biases = math.random([numNeurons, 1], 0, 1); 
+        //console.log("weights: ", this.weights, "biases: ", this.biases);
     }
 
     accumulateGradient(delta, activToTheLeft) {
+        //console.log("delta: ", delta, "activToTheLeft: ", activToTheLeft, "avgWGradients: ", this.avgWGradients, "avgBGradients: ", this.avgBGradients);
         this.avgBGradients = math.add(this.avgBGradients, delta);
         this.avgWGradients = math.add(this.avgWGradients,
              math.multiply(delta, math.transpose(activToTheLeft))); // pt a obtine o matrice de NL x NL-1
     }
 
     subtractFromWeights(valueMatrix) {
+        valueMatrix = valueMatrix.toArray();
+        console.log("weights: ", this.weights);
         this.weights = math.subtract(this.weights, valueMatrix);
+        console.log("after substraction: ", this.weights);
     }
 
     subtractFromBiases(valueMatrix) {
+        valueMatrix = valueMatrix.toArray();
+        console.log("biases: ", this.biases);
+        console.log("valueMatrix: ", valueMatrix);
         this.biases = math.subtract(this.biases, valueMatrix);
+        console.log("after substraction: ", this.biases);
     }
 
 // ---------------------------------- GETTERS -----------------------------------------
+    getWeights() {
+        return this.weights;
+    }
+    
+    getBiases() {
+        return this.biases;
+    }
+
     getActivationFunction() {
         return this.activFunction;
     }
--- obj_Library/simplifyed_model/simpleNetwork.js
+++ obj_Library/simplifyed_model/simpleNetwork.js
@@ -2,20 +2,24 @@ class SmpNetwork {
     constructor(size) {
         this.size = size; // hid + output (== index of Output Layer)
         this.network = new Array(size + 1); // 0 is input layer
-        
     }
 
     initializeLayer(index, size, connections, activFunction) {
         this.network[index] = new SmpLayer(size, connections, activFunction);
+        //console.log("Layer ", index, " initialized with ", size, " neurons");
+        //console.log("Weights: ", this.network[index].getWeights(), "Biases: ", this.network[index].getBiases());
     }
     
     feedForward(input) {  // Works!
-        let activation = input;
+        let activation = math.reshape(input, [784, 1]);
         for (let layerIndex = 1; layerIndex <= this.size; ++layerIndex) {
+            //console.log("Layer ", layerIndex, " input: ", activation);
             let w = this.network[layerIndex].getWeights();
             let b = this.network[layerIndex].getBiases();
+            //console.log(math.size(w), math.size(activation), math.size(b)); 
             activation = math.map(math.add(math.multiply(w, activation), b), this.network[layerIndex].getActivationFunction());
         }
+        //console.log("Output: ", activation);
         return activation;
     }   
 
@@ -25,7 +29,7 @@ class SmpNetwork {
             trainingIndex.push(i);
         }
         
-        for (let i = 1; i <= epochs; ++i) {
+        for (let i = 1; i <= 1/*epochs*/; ++i) {
             trainingIndex.sort(() => Math.random() - 0.5);
 
             let miniBatches = [];
@@ -33,12 +37,13 @@ class SmpNetwork {
                 let miniBatch = trainingIndex.slice(j, j + miniBatchSize);
                 miniBatches.push(miniBatch);
             }
+            //console.log("Epoch: ", i, "MiniBatches: ", miniBatches);
             // update
             for (let j = 0; j < miniBatches.length; ++j) {
                 this.update(miniBatches[j], learningRate, trainingImages, trainingLabels);
             }
             // evaluate
-            console.log("epoch: ", i, "total: ", numInputs, "; guessed: ", this.evaluate());
+            console.log("epoch: ", i, "total: ", testImages.length, "; guessed: ", this.evaluate(testImages, testLabels));
         }
     }
 
@@ -52,10 +57,10 @@ class SmpNetwork {
         }
         // update-itself
         for (let layerIndex = 1; layerIndex <= this.size; ++layerIndex) {
-            let currentLayer = this.network[layerIndex]
+            let currentLayer = this.network[layerIndex],
                 avgWGradients = math.multiply(currentLayer.getWeightsGradient(), learningRate / miniBatch.length),
                 avgBGradients = math.multiply(currentLayer.getBiasesGradient(), learningRate / miniBatch.length);
-            
+            //console.log("Layer ", layerIndex, " avgWGradients: ", avgWGradients, " avgBGradients: ", avgBGradients);
             currentLayer.subtractFromWeights(avgWGradients);
             currentLayer.subtractFromBiases(avgBGradients);  
         }
@@ -63,43 +68,44 @@ class SmpNetwork {
 
     backpropagate(input, label) {
         // forward pass
-        let activation = input;
+        let activation =  math.reshape(input, [784, 1]);
         let activations = [];
-        let weightedSums = [];
-        activation.push(null);
-        weightedSums.push(null);
+        activations.push(activation);
         for (let layerIndex = 1; layerIndex <= this.size; ++layerIndex) {
             let w = this.network[layerIndex].getWeights();
             let b = this.network[layerIndex].getBiases();
-            let weightedSum = math.add(math.multiply(w, activation), b);
-            activation = math.map(weightedSum, this.network[layerIndex].getActivationFunction());
-            weightedSums.push(weightedSum);
+            let weightedSum = math.matrix(math.add(math.multiply(w, activation), b));
+            activation = math.matrix(math.map(weightedSum, this.network[layerIndex].getActivationFunction()));
             activations.push(activation);
         }
         // backward pass
         let delta = math.dotMultiply(
                     this.costDerivative(activations[this.size], label)
-                    , math.map(weightedSums[this.size], derivate(this.network[this.size].getActivationFunction())));
-        this.network[this.size].accumulateGradient(delta);
+                    , math.map(activations[this.size], derivate(this.network[this.size].getActivationFunction())));
+        this.network[this.size].accumulateGradient(delta, activations[this.size - 1]);
         for (let layerIndex = this.size - 1; layerIndex > 0; --layerIndex) {
             let w = this.network[layerIndex + 1].getWeights(); // w of L + 1
-            let primeWs = math.map(weightedSums[layerIndex], derivate(this.network[layerIndex].getActivationFunction()));
+            let primeWs = math.map(activations[layerIndex], derivate(this.network[layerIndex].getActivationFunction()));
             delta = math.dotMultiply(math.multiply(math.transpose(w), delta), primeWs);
-            this.network[layerIndex].accumulateGradient(delta);
+            this.network[layerIndex].accumulateGradient(delta, activations[layerIndex - 1]);
         }
     }
 
     costDerivative(outputActivations, label) {
         let y = math.matrix(math.zeros(10, 1)); // hot encoded
         y.set([label, 0], 1);
-        return math.multiply(math.subtract(outputActivations, y), 2);
+        //console.log (math.subtract(outputActivations, y));
+        return math.subtract(outputActivations, y);
+        //return math.multiply(math.subtract(outputActivations, y), 2);
     }
 
     evaluate(testImages, testLabels) {
         let total = testImages.length, correct = 0 
         for (let i = 0; i < total; ++i) {
             let output = this.feedForward(testImages[i]);
-            let guess = math.argmax(output);
+            //console.log( this.feedForward(testImages[i]), "Output: ", output);
+            let guess = outputDigit(output);
+            //console.log("Guess: ", guess, "Label: ", testLabels[i]);
             if (guess === testLabels[i]) {
                 ++correct
             }
